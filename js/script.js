6ef75ec4ae8b2cc630974a36495abe44650a45a0
[omar dahdouh] <[omar.m.dahdouh@gmail.com]>
2020-01-28 15:13:05 +0200

    update script and testing relates #9

--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@
 /*
 // testing
 let products = [
-    {id:1, name:"kia sol", detail:"medium", price:25000.0, image:"mercedes.jpg", category:1},
-    {id:2, name:"HP laptop", detail:"black", price:600.0, image:"toshiba-1.png", category:2},
-    {id:3, name:"Galaxy S7", detail:"samsung", price:200.0, image:"samsung-2.jpg", category:3}
+    {id:1, name:"kia sol", details:"medium", price:25000.0, image:"mercedes.jpg", category:1},
+    {id:2, name:"HP laptop", details:"black", price:600.0, image:"toshiba-1.png", category:2},
+    {id:3, name:"Galaxy S7", details:"samsung", price:200.0, image:"samsung-2.jpg", category:3}
 ];*/
 
 
@@ -16,12 +16,12 @@ let products = [];
 //make a product as object
 //example >>
 //  let newProduct = makeProduct(3, "Galaxy S7", "samsung", 200.0, "samsung-2.jpg", 3)
-//  return >> {id:3, name:"Galaxy S7", detail:"samsung", price:200.0, image:"samsung-2.jpg", category:3}
-function makeProduct(id, name, detail, price, image, category) {
-    return {id, name, detail, price, image, category};
+//  return >> {id:3, name:"Galaxy S7", details:"samsung", price:200.0, image:"samsung-2.jpg", category:3}
+function makeProduct(id, name, details, price, image, category) {
+    return {id, name, details, price, image, category};
 }
 
-// return new copy of array with the new object
+// return a new copy of the array with the new object
 // example :
 //  let newProduct = makeProduct(3, "Galaxy S7", "samsung", 200.0, "samsung-2.jpg", 3)
 //  products = addProduct(products, newProduct);
@@ -29,8 +29,16 @@ function addProduct(arr, obj) {
     return [...arr, obj];
 }
 
+// return a new copy of the array without the target object
+// example : delete product 'target' from products array
+//  products = deleteProduct(products, target);
+function deleteProduct(arr, id) {
+    return arr.filter(pro => pro.id!=id);
+}
+
 module.exports = {
     makeProduct,
     addProduct,
+    deleteProduct
   }
   
--- test/ecommerce.test.js
+++ test/ecommerce.test.js
@@ -1,24 +1,56 @@
 const {
   makeProduct,
   addProduct,
+  deleteProduct,
 } = require('../js/script.js');
-
 
 test('makeProduct should return an object.', function () {
   let actual = makeProduct(3, "Galaxy S7", "samsung", 200.0, "samsung-2.jpg", 3);
-  let expected = {id:3, name:"Galaxy S7", detail:"samsung", price:200.0, image:"samsung-2.jpg", category:3}
+  let expected = {id:3, name:"Galaxy S7", details:"samsung", price:200.0, image:"samsung-2.jpg", category:3}
 
   expect(actual).toEqual(expected);
 });
 
-test('addProduct() should not changing the origional array.', function () {
-  let currentProducts = [];
-  let oldProducts = [...currentProducts];
-  addProduct(currentProducts, {a:1, b:2, c:3});
+////////////////////////////  addProduct ///////////////////////////////////
 
+test('addProduct() should not mutate the origional array', function () {
+  let currentProducts = [{id:1, val:'hello'}, {id:2, val:'world'}];
+  let oldProducts = [...currentProducts];
+  
+  addProduct(currentProducts, {id:3, val:'hi'});
   expect(oldProducts).toEqual(currentProducts);
 });
 
+test('addProduct() should add the object to the array', function () {
+  let oldProducts = [{id:1, val:'hello'}];
+  let newProducts = addProduct(oldProducts, {id:2, val:'world'});
+  
+  expect(newProducts).toEqual([{id:1, val:'hello'}, {id:2, val:'world'}]);
+});
+
+////////////////////////////  delete ///////////////////////////////////
+
+test('deleteProduct() should not mutate the origional array', function () {
+  let currentProducts = [{id:1, val:'hello'}, {id:2, val:'world'}];
+  
+  let oldProducts = [...currentProducts];
+  deleteProduct(currentProducts, 1);
+  expect(oldProducts).toEqual(currentProducts);
+  deleteProduct(currentProducts, 2);
+  expect(oldProducts).toEqual(currentProducts);
+  deleteProduct(currentProducts, 3);
+  expect(oldProducts).toEqual(currentProducts);
+});
+
+test('deleteProduct() should remove the object from the array', function () {
+  let currentProducts = [{id:1, val:'hello'}, {id:2, val:'world'}];
+  
+  let del1 = deleteProduct(currentProducts, 1);
+  let del2 = deleteProduct(currentProducts, 2);
+
+  expect(del1).toEqual([{id:2, val:'world'}]);
+  expect(del2).toEqual([{id:1, val:'hello'}]);
+});
 
 
 // let obj = {a:1};
