eb53b36f74a07b61a5f221f101fca9c0b7d917f8
[omar dahdouh] <[omar.m.dahdouh@gmail.com]>
2020-01-28 14:37:07 +0200

    updating test and script relates #9

--- test/ecommerce.test.js
+++ test/ecommerce.test.js
@@ -4,7 +4,6 @@ const {
 } = require('../js/script.js');
 
 
-
 test('makeProduct should return an object.', function () {
   let actual = makeProduct(3, "Galaxy S7", "samsung", 200.0, "samsung-2.jpg", 3);
   let expected = {id:3, name:"Galaxy S7", detail:"samsung", price:200.0, image:"samsung-2.jpg", category:3}
